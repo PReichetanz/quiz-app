4060d3e4997258d2b0ce2090002970ba0b12fd1f
PReichetanz <paul.reichetanzgmx.de>
2021-10-12 17:26:25 +0200

    Fix class names for js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const toggleAnswerButton = document.querySelector(
-  ".card__toggle-answer-button"
-);
-const answer = document.querySelector(".card__answer-text");
-const answerHeading = document.querySelector(".card__answer-heading");
+const toggleAnswerButton = document.querySelector(".js-toggle-answer");
+const answer = document.querySelector(".js-answer-text");
+const answerHeading = document.querySelector(".js-answer-heading");
 
 toggleAnswerButton.addEventListener("click", () => {
   answerHeading.classList.toggle("hidden");
