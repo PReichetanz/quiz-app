5b3a79bbfcea400d2c0eb874809d399d4ee2daa7
PReichetanz <paul.reichetanzgmx.de>
2021-10-12 20:18:22 +0200

    Add js buttons to navigation => create one-page-js

--- index.js
+++ index.js
@@ -16,3 +16,70 @@ const bookmark = document.querySelector(".js-bookmark");
 bookmark.addEventListener("click", () => {
   bookmark.classList.toggle("card__bookmark--active");
 });
+
+/* CHANGE NAVIGATION TAB */
+
+const homeButton = document.querySelector(".js-home-button");
+const bookmarksButton = document.querySelector(".js-bookmark-button");
+const createButton = document.querySelector(".js-create-button");
+const profileButton = document.querySelector(".js-profile-button");
+
+const homeSection = document.querySelector(".js-questions");
+const bookmarkSection = document.querySelector(".js-bookmarks");
+const createSection = document.querySelector(".js-create");
+const profileSection = document.querySelector(".js-profile");
+
+const homeIcon = document.querySelector(".js-home-icon");
+const bookmarkIcon = document.querySelector(".js-bookmark-icon");
+const createIcon = document.querySelector(".js-create-icon");
+const profileIcon = document.querySelector(".js-profile-icon");
+
+homeButton.addEventListener("click", () => {
+  homeSection.classList.remove("hidden");
+  bookmarkSection.classList.add("hidden");
+  createSection.classList.add("hidden");
+  profileSection.classList.add("hidden");
+
+  homeIcon.classList.add("nav-item--active");
+  bookmarkIcon.classList.remove("nav-item--active");
+  createIcon.classList.remove("nav-item--active");
+  profileIcon.classList.remove("nav-item--active");
+});
+
+bookmarksButton.addEventListener("click", () => {
+  homeSection.classList.add("hidden");
+  bookmarkSection.classList.remove("hidden");
+  createSection.classList.add("hidden");
+  profileSection.classList.add("hidden");
+
+  homeIcon.classList.remove("nav-item--active");
+  bookmarkIcon.classList.add("nav-item--active");
+  createIcon.classList.remove("nav-item--active");
+  profileIcon.classList.remove("nav-item--active");
+});
+
+createButton.addEventListener("click", () => {
+  homeSection.classList.add("hidden");
+  bookmarkSection.classList.add("hidden");
+  createSection.classList.remove("hidden");
+  profileSection.classList.add("hidden");
+
+  homeIcon.classList.remove("nav-item--active");
+  bookmarkIcon.classList.remove("nav-item--active");
+  createIcon.classList.add("nav-item--active");
+  profileIcon.classList.remove("nav-item--active");
+});
+
+profileButton.addEventListener("click", () => {
+  homeSection.classList.add("hidden");
+  bookmarkSection.classList.add("hidden");
+  createSection.classList.add("hidden");
+  profileSection.classList.remove("hidden");
+
+  homeIcon.classList.remove("nav-item--active");
+  bookmarkIcon.classList.remove("nav-item--active");
+  createIcon.classList.remove("nav-item--active");
+  profileIcon.classList.add("nav-item--active");
+});
+
+/* SET NAVIGATION ACTIVE BUTTON */
