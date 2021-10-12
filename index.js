196ad732ad979357af9982c8fc1e46b8115cf77d
PReichetanz <paul.reichetanzgmx.de>
2021-10-12 19:22:51 +0200

    Add bookmark toggle of first card

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* TOGGLE ANSWER ON QUESTION CARD */
+
 const toggleAnswerButton = document.querySelector(".js-toggle-answer");
 const answer = document.querySelector(".js-answer-text");
 const answerHeading = document.querySelector(".js-answer-heading");
@@ -5,4 +7,12 @@ const answerHeading = document.querySelector(".js-answer-heading");
 toggleAnswerButton.addEventListener("click", () => {
   answerHeading.classList.toggle("hidden");
   answer.classList.toggle("hidden");
+});
+
+/* TOGGLE BOOKMARK */
+
+const bookmark = document.querySelector(".js-bookmark");
+
+bookmark.addEventListener("click", () => {
+  bookmark.classList.toggle("card__bookmark--active");
 });
